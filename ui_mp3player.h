ba19f5cdea8b5b6e034629ab0e70b3e06c50ee36
Sn4Rs <wisbi1997@gmail.com>
2024-12-22 14:29:34 +0800

    Revert "copy to main"
    This reverts commit 93421ab4adda3c07b1d2d5f44e915133e536a8a0.

--- ui_mp3player.h
+++ ui_mp3player.h
@@ -148,7 +148,7 @@ public:
 
         retranslateUi(mp3Player);
 
-        tabWidget->setCurrentIndex(0);
+        tabWidget->setCurrentIndex(1);
 
 
         QMetaObject::connectSlotsByName(mp3Player);
